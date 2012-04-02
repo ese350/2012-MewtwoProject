d2d73e33bfe979ec8935ba0dc96bb4d754d81e97
Steven Chen <szchen77@gmail.com>
2012-04-02 04:51:39 -0400

    Source for blog

---
+++ octopress/config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/MewtwoProject/"
+http_images_path = "/MewtwoProject/images"
+http_fonts_path = "/MewtwoProject/fonts"
+css_dir = "public/MewtwoProject/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
